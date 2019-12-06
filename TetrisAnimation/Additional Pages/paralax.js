a8baff2ad4d3d1175bd4ebba562d9576368d4f00
Nuno Sousa <9180579@esmad.ipp.pt>
2019-12-06 01:27:15 +0000

    Done

--- TetrisAnimation/Additional Pages/paralax.js
+++ TetrisAnimation/Additional Pages/paralax.js
@@ -2,7 +2,7 @@ let container = document.querySelector('.container')
 let protos = document.querySelectorAll('.proto')
 
 container.addEventListener('mousemove', e => {
-    let move = (e.clientX * 0.05) + 4
+    let move = (e.clientX * 0.00005)
 
     protos.forEach(proto => {
         proto.style.transform = `translateX(${move}%)`
